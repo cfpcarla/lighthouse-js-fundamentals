09a241e1d163fecc4bf928dd8539a15374fca922
Carla <cfpcarla@gmail.com>
2019-10-21 14:29:45 +0000

    Assignments

---
+++ odd.js
@@ -0,0 +1,9 @@
+const isOdd = function(num) {
+    return num % 2 !== 0;
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log("9 is odd: " + isOdd(9));
+console.log("1 is odd: " + isOdd(1));
+console.log("13 is odd: " + isOdd(13));
+console.log("4 is odd: " + isOdd(4));
